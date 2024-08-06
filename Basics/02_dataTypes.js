5e7a9d7bdc6f41682aaad30bc3311c262f11b98e
Valendra07 <124619345+Valendra07@users.noreply.github.com>
2024-08-06 18:10:37 +0000

    JS

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,40 @@
+let myName = "Vale" // String both single and double quotes allowed not in c++
+let age = 25 // Number
+let passed = true // Boolean
+
+/*
+There are 8 datatypes.
+Numbers can be int or 64 bit floating point numbers.
+String
+Boolean
+BigInt
+Undefined variable without value
+Null
+Symbol
+Object
+*/
+
+console.log("Valendra" + 16+4);// Js treats 7 as string
+console.log(16+4+"Valendra")// Js evaluates expression form left to right 
+// In py only string can be concatinated amoung themselves.
+let x = 6;
+x = "Vale" //same variable holding different Datatypes 
+
+// let y = BigInt("Number in string");
+// console.log(y);
+
+let arr = [1,2]
+console.log(typeof(arr))// Type of variable or expression
+
+// Empty variable
+
+arr = undefined
+console.log(typeof(arr))
+
+// let str = ""// It is empty not undefined.
+
+
+
+
+
+
---
+++ Basics/03_typeConversion.js
@@ -0,0 +1,67 @@
+// let numStr = "0"
+// let num = Number(numStr);
+
+// let numStr = null
+// let num = Number(numStr)
+// console.log(num)
+
+// let numStr = true
+// let num = Number(numStr)
+// console.log(num)
+
+// let numStr = "33abc"
+// let num = Number(numStr)
+// console.log(num)
+// console.log(typeof num)
+// or console.log(typeof(num)
+
+// let numStr = ""
+// let num = Number(numStr)
+// console.log(num)
+// console.log(typeof num)
+
+/* Converting to Number
+"33" => 33
+null => 0
+true => 1 false => 0
+"3ad" => NaN Not a number is number datatype
+"" => 0
+*/
+
+
+// Conversion to string 
+// let num = 5
+// let numStr = String(num)
+// console.log(numStr)
+// console.log(typeof numStr)
+
+
+
+// let num = 0
+// let isPassed = Boolean(num)
+// console.log(isPassed)
+// console.log(typeof isPassed)
+
+// let num = 60
+// let isPassed = Boolean(num)
+// console.log(isPassed)
+// console.log(typeof isPassed)
+
+// let num = ""
+// let isPassed = Boolean(num)
+// console.log(isPassed)
+// console.log(typeof isPassed)
+
+let num = null
+let isPassed = Boolean(num)
+console.log(isPassed)
+console.log(typeof isPassed)
+
+
+/* Conversion to Boolean
+ "" => 0
+ "abc" => 1
+ 59 => 1
+ 0 => 0
+ null => 0
+*/
